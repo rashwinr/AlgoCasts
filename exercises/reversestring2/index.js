e31e37c31c61f3148200acd72e43fa7b0460c1f9
rashwinr <ark440@nyu.edu>
2019-12-06 02:22:35 -0500

    Reverse String method 2

---
+++ exercises/reversestring2/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    var reversed = '';
+    for (let x of str) {
+        reversed = x + reversed;
+    }
+    return reversed;
+}
+
+module.exports = reverse;
